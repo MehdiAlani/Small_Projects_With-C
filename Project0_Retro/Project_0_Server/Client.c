09e04fc98b5bd5c75100a6488188364800ba447a
Kyymo <137647536+MehdiAlani@users.noreply.github.com>
2023-10-18 12:02:11 -0200

    18 oct 23

--- Project0_Retro/Project_0_Server/Client.c
+++ Project0_Retro/Project_0_Server/Client.c
@@ -1,34 +1,65 @@
-#include <winsock.h>
+#include <winsock.h>        // -lws2_32 Flag
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <pthread.h>        // -lpThread Flag
+#define GROOM_SERVER_IP "127.0.0.1"
+#define GROOM_SERVER_PORT 80 
 
-int main(){ 
+int ConnectServer(int clientSocket , struct sockaddr_in *Sp){ 
+    if (connect(clientSocket,(struct sockaddr *)Sp,sizeof(*Sp)) == -1) return 0;
+    else return 1;
+}
+void * TreadSend(void * clientSocket){
+    char Msg[4096];
+    while(1){
+        printf("You: ");
+        scanf("%s",&Msg);
+        printf("\n");
+        while(send(*((int *)clientSocket), Msg, sizeof(Msg) - 1, 0)==-1);
+        sleep(0.5);
+    }
+
+}
+void * TreadRecv(void * clientSocket){
+    char buffer[4096];
+    while(1){
+        if(recv(*((int *)clientSocket),buffer , sizeof(buffer),0) != -1) 
+            printf("Server: %s\n",buffer);
+    }
+}
+int main(){
+    // Init The Library
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         fprintf(stderr, "WSAStartup failed\n");
         return 1;
     }
-    int clientSocket = socket(AF_INET,SOCK_STREAM,0);
+    pthread_t Th_Send,Th_Recv;
     struct sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(54928);
-    serverAddress.sin_addr.s_addr = inet_addr("");
-    while(1){
-        if(connect(clientSocket,(struct sockaddr *)&serverAddress,sizeof(serverAddress)) == -1){
-            printf("Failed To Connect To Server\n");
-            sleep(1);
-        }
-        else{
-            printf("Connected To Server\n");
-            break;
-        }
-    }
-    char buffer[1024]; // 1Kb of Memory For Ready Data over the Network
-    if((recv(clientSocket,buffer,sizeof(buffer),0))==-1){
-        printf("Failed to Read from Server\n");
+    serverAddress.sin_port = htons(GROOM_SERVER_PORT);
+    serverAddress.sin_addr.s_addr = inet_addr(GROOM_SERVER_IP);
+
+    char buffer[4096] ,Msg[4096];
+    memset(&Msg,'\0',sizeof(Msg));
+    int clientSocket = socket(AF_INET,SOCK_STREAM,0);
+
+    if(clientSocket == -1){
+        printf("Failed To Create Socket\n");
+        WSACleanup();
         exit(1);
     }
-    printf("From Server :%s\n",buffer);
+
+    while (!ConnectServer(clientSocket,&serverAddress)){
+        printf("Failed To Connect To Server \n");
+        sleep(1);
+    }
+    printf("Connnected To Server \n");
+    pthread_create(&Th_Recv,NULL,&TreadRecv,&clientSocket);
+    pthread_create(&Th_Send,NULL,&TreadSend,&clientSocket);
+    pthread_join(Th_Recv,NULL);
+    pthread_join(Th_Send,NULL);
     return 0;
 }
--- Project0_Retro/Project_0_Server/Server.c
+++ Project0_Retro/Project_0_Server/Server.c
@@ -1,26 +1,32 @@
+#include <winsock.h>        // -lws2_32 Flag
 #include <stdio.h>
-#include <winsock2.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <pthread.h>        // -lpThread Flag
 int main(){
+    // init The 
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         fprintf(stderr, "WSAStartup failed\n");
         return 1;
     }
 
-
-    int ServerSock = socket(AF_INET,SOCK_STREAM,0) , ClientSocket;
+    int ServerSock = socket(AF_INET,SOCK_STREAM,0) , ClientSocket1 ,ClientSocket2;
     if(ServerSock == - 1){
         printf("Failed To Create Socket \n");
         exit(1);
     }
+    
     struct sockaddr_in ClientAddress;
     struct sockaddr_in ServerAddress;
     ServerAddress.sin_family = AF_INET;
     ServerAddress.sin_port = htons(80);
     ServerAddress.sin_addr.s_addr = INADDR_ANY;
+
+    char Msg[4096],buffer[4096];
+    memset(&Msg,'\0',sizeof(Msg));
+
     if(bind(ServerSock,(struct sockaddr *)&ServerAddress,sizeof(ServerAddress)) == -1) {
         printf("Failed To bind\n");
         exit(1);
@@ -31,9 +37,14 @@ int main(){
     }
     printf("Listening on port 80 .. \n");
     int lenAddr = sizeof(ClientAddress);
-    ClientSocket = accept(ServerSock,(struct sockaddr *)&ClientAddress,&lenAddr);
+    ClientSocket1 = accept(ServerSock,(struct sockaddr *)&ClientAddress,&lenAddr);
     printf("Client Connected\n");
-    char Text[] = "Hey From LocalHost:80";
-    send(ClientSocket,Text,sizeof(Text)-1,0);
-    return 0;
+    while(1){
+        printf("You: ");
+        scanf("%s",&Msg);
+        printf("%s\n",Msg);
+        fflush(stdin);
+        send(ClientSocket1,Msg,sizeof(Msg)-1,0);
+        if(recv(ServerSock,buffer,sizeof(buffer),0) != 1) printf("Client: %s\n",buffer);
+    }
 }
